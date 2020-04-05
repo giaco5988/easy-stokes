2f8a5bde3bbee1fa740bed623711ca57603f9862
Giacomo <giaco5988@gmail.com>
2020-04-05 11:51:39 +0200

    plot also final curvature

--- outputAndEvent/outfun_newton.m
+++ outputAndEvent/outfun_newton.m
@@ -1,12 +1,12 @@
 function stop = outfun_newton(x,optimValues,state,nonLinearFunction)
 
 %% COMPUTE NONLINEAR FUNCTION (GETS CURRENT SHAPE)
-[~,~,~,xBaseGrid,yBaseGrid,~,xBase,yBase] = nonLinearFunction(x);
+[~,~,~,xGrid,yGrid,~,xBase,yBase] = nonLinearFunction(x);
 
 %% PLOT CURRENT SHAPE
 if state == 'iter'
     figure(1)
-    plot([xBaseGrid, flip(xBaseGrid)],[yBaseGrid, -flip(yBaseGrid)])
+    plot([xGrid, flip(xGrid)],[yGrid, -flip(yGrid)])
     hold on
     axis equal
     plot([xBase; flip(xBase)],[yBase; -flip(yBase)],'--')
@@ -21,9 +21,21 @@ end
 if state == 'iter'
     figure(2)
     res = norm(optimValues.fval,inf);
-    semilogy(optimValues.iteration,res,'ok')
+    semilogy(optimValues.iteration,res,'or','MarkerSize',10)
     hold on
+    semilogy(optimValues.iteration,res,'.k','MarkerSize',30)
     xyLabelTex('\mathrm{iteration}','||R||_\infty')
+    grid on
+end
+
+%% PLOT CURVATURE OF THE FINAL SHAPE
+if state=='done'
+    figure(3)
+    [k1,k2] = computeCurvatureSplines(xGrid,yGrid,1);
+    k = k1+k2;
+    plot(xGrid,k)
+    xyLabelTex('z','K')
+    title('Curvature')
     grid on
 end
 
